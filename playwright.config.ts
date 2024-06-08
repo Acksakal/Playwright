19f9f924575ff4bdcd2ac280dbefd82b9f193f36
Daniyar <147915354+Acksakal@users.noreply.github.com>
2024-06-08 16:51:24 +0500

    change headless mode
    changed headless mode to headed by changing true to false of corresponding property in the config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
       use: {
         ...devices['Desktop Chrome'],
         launchOptions: {
-          headless: true
+          headless: false
         }
        },
     },
